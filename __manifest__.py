f3f9cd0d7a3beb0a39e4a0c242683e7dff6bda14
M0-V4 <maguardia2006@gmail.com>
2025-12-01 11:49:31 +0100

    codigo

--- __manifest__.py
+++ __manifest__.py
@@ -1,34 +1,33 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "autoescuela",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'name': "Autoescuela",
+    'summary': "Gestión de autoescuelas, profesores, alumnos y exámenes",
     'description': """
-Long description of module's purpose
+    Módulo de gestión de autoescuelas para la práctica UT03.
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'author': "Tu nombre / vuestro grupo",
+    'website': "https://github.com/tuusuario/autoescuela",
+    'category': 'Education',
+    'version': '1.0',
+    'application': True,
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/autoescuela_security.xml',
+        'security/ir.model.access.csv',
+        'data/autoescuela_sequence.xml',
+        'views/autoescuela_menus.xml',
+        'views/autoescuela_autoescuela_view.xml',
+        'views/autoescuela_profesor_view.xml',
+        'views/autoescuela_alumno_view.xml',
+        'views/autoescuela_examen_view.xml',
+        'reports/autoescuela_autoescuela_report.xml',
+        'reports/autoescuela_profesor_report.xml',
+        'reports/autoescuela_alumno_report.xml',
+        'reports/autoescuela_examen_report.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        # 'demo/demo.xml',
     ],
 }
+
 

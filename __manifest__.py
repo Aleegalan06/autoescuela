5979a6c6d5cb9ef380ca0bc3420803f1a422c5a1
M0-V4 <maguardia2006@gmail.com>
2025-12-02 09:35:18 +0100

    models.py hecho

--- __manifest__.py
+++ __manifest__.py
@@ -12,21 +12,12 @@
     'application': True,
     'depends': ['base'],
     'data': [
-        'security/autoescuela_security.xml',
         'security/ir.model.access.csv',
-        'data/autoescuela_sequence.xml',
-        'views/autoescuela_menus.xml',
-        'views/autoescuela_autoescuela_view.xml',
-        'views/autoescuela_profesor_view.xml',
-        'views/autoescuela_alumno_view.xml',
-        'views/autoescuela_examen_view.xml',
-        'reports/autoescuela_autoescuela_report.xml',
-        'reports/autoescuela_profesor_report.xml',
-        'reports/autoescuela_alumno_report.xml',
-        'reports/autoescuela_examen_report.xml',
+        'views/templates.xml',
+        'views/views.xml',
     ],
     'demo': [
-        # 'demo/demo.xml',
+        'demo/demo.xml',
     ],
 }
 

df5a2e226baa898b4e9c8b70d74a84fc9a925557
Aleegalan06 <alejandrogalanlavellaned@gmail.com>
2025-12-09 08:31:34 +0000

    grupos gestor y director

--- __manifest__.py
+++ __manifest__.py
@@ -5,28 +5,20 @@
     'description': """
     Módulo de gestión de autoescuelas para la práctica UT03.
     """,
-    'author': "Tu nombre / vuestro grupo",
-    'website': "https://github.com/tuusuario/autoescuela",
+    'author': "Miguel Angel Guardia, David Resino, Alejandro Galan",
+    'website': "https://github.com/Aleegalan06/autoescuela",
     'category': 'Education',
     'version': '1.0',
     'application': True,
     'depends': ['base'],
     'data': [
-        'security/autoescuela_security.xml',
+        'security/autoescuela_groups.xml',
         'security/ir.model.access.csv',
-        'data/autoescuela_sequence.xml',
-        'views/autoescuela_menus.xml',
-        'views/autoescuela_autoescuela_view.xml',
-        'views/autoescuela_profesor_view.xml',
-        'views/autoescuela_alumno_view.xml',
-        'views/autoescuela_examen_view.xml',
-        'reports/autoescuela_autoescuela_report.xml',
-        'reports/autoescuela_profesor_report.xml',
-        'reports/autoescuela_alumno_report.xml',
-        'reports/autoescuela_examen_report.xml',
+        'views/templates.xml',
+        'views/views.xml',
     ],
     'demo': [
-        # 'demo/demo.xml',
+        'demo/demo.xml',
     ],
 }
 
